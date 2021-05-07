b3f98d56033f963e7df6508fd565ee7913f6cf05
Gaku Sato <red.blues.quartet@gmail.com>
2021-05-07 21:54:45 +0900

    first commit

---
+++ printuserid.py
@@ -0,0 +1,31 @@
+# 使用するライブラリをインポート
+import tweepy
+
+# 取得したAPIキーを入力
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+
+# フォローリスト取得処理
+cursor = -1
+while cursor != 0:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    # "XXX"には取得元のアカウント名を入力
+    itr = tweepy.Cursor(api.friends_ids, id='@XXX', cursor=cursor).pages()
+    try:
+        for friends_ids in itr.next():
+            try:
+                user = api.get_user(friends_ids)
+                user_info = [user.screen_name]
+                print(user_info)
+            # tweepyでエラーが発生時の例外処理
+            except tweepy.error.TweepError as e:
+                print(e.reason)
+    # 接続エラー発生時の例外処理
+    except ConnectionError as e:
+        print(e.reason)
+    cursor = itr.next_cursor
