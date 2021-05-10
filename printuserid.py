6a1be324bd015c9b614537d72d28b46ae7557178
Gaku Sato <red.blues.quartet@gmail.com>
2021-05-10 19:18:51 +0900

    remove files

--- printuserid.py
+++ printuserid.py
@@ -2,11 +2,10 @@
 import tweepy
 
 # 取得したAPIキーを入力
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
-
+CONSUMER_KEY = "4DFeL8Ilu5bJ9jrnl5izH9I66"
+CONSUMER_SECRET = "wsS9zxhjz1V32bKEDfVFuv77r1FqlvQwls7fFBU2ZUl5LvDcAc"
+ACCESS_TOKEN = "832566770162556929-gFsjjU3LOmQiJPz4HF0KSNau5wOvy7E"
+ACCESS_TOKEN_SECRET = "fMFqxFRg9yBWfXqC5AU2Zu0oAwNIMnQgWcPH1i81ZJ0lo"
 
 # フォローリスト取得処理
 cursor = -1
@@ -15,7 +14,7 @@ while cursor != 0:
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True)
     # "XXX"には取得元のアカウント名を入力
-    itr = tweepy.Cursor(api.friends_ids, id='@XXX', cursor=cursor).pages()
+    itr = tweepy.Cursor(api.friends_ids, id='@kooooook1215', cursor=cursor).pages()
     try:
         for friends_ids in itr.next():
             try:
